ab3b620d65fa437468143f99e83f8448eeaa360d
EGOIST <0x142857@gmail.com>
2017-05-31 10:17:34 -0500

    :art: A couple of build tweaks (#2)
    * copy is uncessary
    
    * auto-copy CNAME

--- poi.config.js
+++ poi.config.js
@@ -1,7 +1,6 @@
 module.exports = (options, req) => ({
   entry: './src/index.js',
   homepage: './', // overwrite homepage from package.json (to use relative URLs)
-  copy: { from: './static/*', to: './dist/*' },
   html: {
     template: './index.html'
   }
