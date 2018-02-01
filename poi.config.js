27cc4d31321e70a6902f53d74232fdbc12ddbf42
timpur <timothy@purchas.com>
2018-02-01 20:36:21 +1100

    Revamp

--- poi.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = (options, req) => ({
-  entry: './src/index.js',
-  homepage: './', // overwrite homepage from package.json (to use relative URLs)
-  html: {
-    template: './index.html'
-  }
-})
